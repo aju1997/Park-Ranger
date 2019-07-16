3b0326432fcb01a582f5423a93d0b7c5977dda12
Anderson Ju <aju001@ucr.edu>
2019-07-16 08:35:08 -0700

    all states added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,57 +7,63 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => new _HomePageState();
 }
 final states = [
-    'National Park',
+    'National Parks',
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
     'California',
-    'Washington',
-    'Nevada',
-    'Florida',
-    'Wyoming',
     'Colorado',
-    'Montana',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
     'Indiana',
-    'Iowa', 
-    'Kansas', 
-    'Kentucky', 
-    'Louisiana', 
-    'Maine', 
-    'Maryland', 
-    'Massachusetts', 
-    'Michigan', 
-    'Minnesota', 
-    'Mississippi', 
-    'Missouri', 
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
     'Montana',
-    'Nebraska', 
-    'New Hampshire', 
-    'New Jersey', 
-    'New Mexico', 
-    'New York', 
-    'North Carolina', 
-    'North Dakota', 
-    'Ohio', 
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Carolina',
+    'North Dakota',
+    'Ohio',
     'Oklahoma',
     'Oregon',
     'Pennsylvania',
     'Rhode Island',
-    'South Carolina', 
-    'South Dakota', 
-    'Tennessee', 
-    'Texas', 
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
     'Utah',
     'Vermont',
     'Virginia',
     'Washington',
-    'West Virginia', 
-    'Wisconsin',  
-  ];
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming',
+];
+
   final searchresult = [];
 
 class _HomePageState extends State<HomePage> {
   
-
-  String _search = '';
-  int length = 0;
   @override
   Widget build(BuildContext context) {
     // Scaffold generates the material content,
@@ -176,90 +182,3 @@ void search(String searchText) {
     }
   }
 }
-
-//     return Scaffold(
-//       body: Center(
-//         child: ListView(
-//           shrinkWrap: true,
-//           padding: EdgeInsets.only(left: 24.0, right: 24.0),
-//           children: <Widget>[
-//             logo,
-//             SizedBox(height: 48.0)
-//           ],
-//         ),
-//       ),
-//     );
-
-//   }
-//   @override
-//   Widget _myListView(BuildContext context) {
-//     final states = ['National Park', 'California', 'Oregon', 'Washington', 'Nevada', 'Florida', 'Wyoming', 'Colorado', 'Montana'];
-
-//     return ListView.builder(
-//       itemCount: states.length,
-//       itemBuilder: (context,index) {
-//         return ListTile(
-//           title: Text(states[index]),
-//         );
-//       },
-//     );
-//   }
-// }
-
-// class HomePage extends StatelessWidget {
-//   static String tag = 'home-page';
-
-//   @override
-//   Widget build(BuildContext context) {
-//   //   final alucard = Hero(
-//   //     tag: 'hero',
-//   //     child: Padding(
-//   //       padding: EdgeInsets.all(16.0),
-//   //       child: CircleAvatar(
-//   //         radius: 72.0,
-//   //         backgroundColor: Colors.transparent,
-//   //         backgroundImage: AssetImage('assets/nps.png'),
-//   //       ),
-//   //     ),
-//   //   );
-
-//   //   final welcome = Padding(
-//   //     padding: EdgeInsets.all(8.0),
-//   //     child: Text(
-//   //       'Welcome Alucard',
-//   //       style: TextStyle(fontSize: 28.0, color: Colors.white),
-//   //     ),
-//   //   );
-
-//   //   final lorem = Padding(
-//   //     padding: EdgeInsets.all(8.0),
-//   //     child: Text(
-//   //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit condimentum mauris id tempor. Praesent eu commodo lacus. Praesent eget mi sed libero eleifend tempor. Sed at fringilla ipsum. Duis malesuada feugiat urna vitae convallis. Aliquam eu libero arcu.',
-//   //       style: TextStyle(fontSize: 16.0, color: Colors.white),
-//   //     ),
-//   //   );
-
-//   //   final body = Container(
-//   //     width: MediaQuery.of(context).size.width,
-//   //     padding: EdgeInsets.all(28.0),
-//   //     decoration: BoxDecoration(
-//   //       gradient: LinearGradient(colors: [
-//   //         Colors.blue,
-//   //         Colors.lightBlueAccent,
-//   //       ]),
-//   //     ),
-//   //     child: Column(
-//   //       children: <Widget>[alucard, welcome, lorem],
-//   //     ),
-//   //   );
-
-//   return Scaffold(
-//     appBar: AppBar(
-//       backgroundColor: Colors.green[400],
-//       centerTitle: true,
-//       title: Text('Park Ranger')
-//     ),
-//     // body: body
-//   );
-//   }
-// }
